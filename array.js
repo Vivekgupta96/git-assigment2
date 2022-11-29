b0f539fda93efee53347b4fdd50e4444ddef6aa7
vivekgupta96 <vivekg0691@gmail.com>
2022-11-29 20:16:37 +0530

    array and string

---
+++ array.js
@@ -0,0 +1,9 @@
+// print all the char of string
+
+let str="abcdefghijklmnopqrstuvwxyz"
+for(i=0;i<str.length;i++){
+
+
+    
+}
+
---
+++ string.js
@@ -0,0 +1,8 @@
+// print all the element of array
+
+let arr=[1,2,3,4,5,6,7,8,9]
+
+for(i=o;i<arr.length;i++){
+
+
+}
